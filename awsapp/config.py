bc15a2335433891c4d3ef15574970a336f32a83b
Ian Johnson <enjalot+github@gmail.com>
2009-09-01 11:14:03 -0400

    modified config to be a little smarter, added .gitignore

--- awsapp/config.py
+++ awsapp/config.py
@@ -1,5 +1,10 @@
 import ConfigParser
+try:
+    import settings
+    conf_path = settings.AWS_CONF_PATH
+except:
+    conf_path = 'awsapp/aws.conf'
 config = ConfigParser.ConfigParser()
-config.read("awsapp/aws.conf")
+config.read(conf_path)
 aws_config = dict(config.items('aws'))
 crypt_config = dict(config.items('crypt'))
