bf8e541c142b2847562515c05124286c0332b2ac
David Arthur <mumrah@gmail.com>
2009-09-03 22:06:39 -0400

    You got your django in my framework

--- alerts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('motoproto.alerts.views',
-    url(r'^$', 'alerts', name="alerts"),
-    url(r'^create$', 'create', name="create"),
-)
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('motoproto.users.views',
-    #url(r'^$', 'user_list', name="users"),
-    #url(r'^list_datagrid/$', 'user_list_datagrid', name="user-list-datagrid"),
-    url(r'^register/$', 'register', name="register"),
-    #url(r'^(?P<userid>\d+)/$', 'profile', name="user-profile"),
-    #url(r'^edit/(?P<userid>\d+)/$', 'edit_profile', name="user-edit-profile"),
-)
